c0bc1109eccc677cc6c630027d1c7d930cf05e64
GarvinDholakiya <garvindholakiya511@gmail.com>
2025-07-14 23:42:31 +0530

    ui push 2

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<!-- Mirrored from template.dsngrid.com/JinnityAI/dark/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 12 Jul 2025 15:58:22 GMT -->
+<!-- Mirrored from template.dsngrid.com/JinnityAI/dark/site/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 12 Jul 2025 15:58:22 GMT -->
 
 <head>
   <!-- Required meta tags -->
@@ -42,99 +42,70 @@
   </div>
 
   <!-- Nav Bar -->
-  <div class="dsn-nav-bar">
-    <div class="site-header">
-      <div class="extend-container">
-        <div class="inner-header">
-          <div class="main-logo">
+    <div class="dsn-nav-bar">
+      <div class="site-header">
+        <div class="extend-container">
+          <div class="inner-header">
+            <div class="main-logo">
+              <a href="index.html">
+                <img class="dark-logo" src="assets/img/logo-dark.png" alt="" />
+                <h4 style="font-weight: 600">JinnityAI</h4>
+              </a>
+            </div>
+          </div>
+          <nav class="accent-menu main-navigation">
+            <ul class="extend-container">
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <!-- <li><a href="index.html#about">About Us</a></li> -->
+              <li><a href="/form">Query</a></li>
+              <li><a href="site/sa100.html">SA100</a></li>
+              <li><a href="site/contact.html">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="header-top header-top-hamburger">
+        <div class="header-container">
+          <div class="logo main-logo">
             <a href="index.html">
-              <img class="dark-logo" src="{{ asset('/site/assets/img/logo-dark.png') }}" alt="" />
+              <img class="dark-logo" src="assets/img/logo-dark.png" alt="" />
               <h4 style="font-weight: 600">JinnityAI</h4>
             </a>
           </div>
-        </div>
-        <nav class="accent-menu main-navigation">
-          <ul class="extend-container">
-            <li class="custom-drop-down">
-              <a href="#">Home</a>
-              <ul>
-                <li><a href="index.html">Main Demo</a></li>
-                <li><a href="one-page-3.html">One Page Three</a></li>
-                <li><a href="one-page-4.html">One Page Four</a></li>
-              </ul>
-            </li>
-            <li class="custom-drop-down">
-              <a href="#">Slider</a>
-              <ul>
-                <li><a href="slider.html">Slider</a></li>
-                <li><a href="slider2.html">Slider two</a></li>
-                <li><a href="slider3.html">Slider Three</a></li>
-              </ul>
-            </li>
-            <li><a href="about.html">About</a></li>
-            <li class="custom-drop-down">
-              <a href="#">Work</a>
-              <ul>
-                <li><a href="work.html">Work</a></li>
-                <li><a href="project-9.html">Header One</a></li>
-                <li><a href="project-7.html">Header Two</a></li>
-                <li><a href="project-5.html">Header Three</a></li>
-              </ul>
-            </li>
-            <li class="custom-drop-down">
-              <a href="#">Blog</a>
-              <ul>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="post.html">Single Post</a></li>
-              </ul>
-            </li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-    <div class="header-top header-top-hamburger">
-      <div class="header-container">
-        <div class="logo main-logo">
-          <a href="index.html">
-            <img class="dark-logo" src="{{ asset('/site/assets/img/logo-dark.png') }}" alt="" />
-            <h4 style="font-weight: 600">JinnityAI</h4>
-          </a>
-        </div>
 
-        <div class="menu-icon" data-dsn="parallax" data-dsn-move="5">
-          <div class="icon-m">
-            <i class="menu-icon-close fas fa-times"></i>
-            <span class="menu-icon__line menu-icon__line-left"></span>
-            <span class="menu-icon__line"></span>
-            <span class="menu-icon__line menu-icon__line-right"></span>
+          <div class="menu-icon" data-dsn="parallax" data-dsn-move="5">
+            <div class="icon-m">
+              <i class="menu-icon-close fas fa-times"></i>
+              <span class="menu-icon__line menu-icon__line-left"></span>
+              <span class="menu-icon__line"></span>
+              <span class="menu-icon__line menu-icon__line-right"></span>
+            </div>
+
+            <div class="text-menu">
+              <div class="text-button">Menu</div>
+              <div class="text-open">Open</div>
+              <div class="text-close">Close</div>
+            </div>
           </div>
 
-          <div class="text-menu">
-            <div class="text-button">Menu</div>
-            <div class="text-open">Open</div>
-            <div class="text-close">Close</div>
+          <div class="nav">
+            <div class="inner">
+              <div class="nav__content"></div>
+            </div>
           </div>
-        </div>
-
-        <div class="nav">
-          <div class="inner">
-            <div class="nav__content"></div>
-          </div>
-        </div>
-        <div class="nav-content">
-          <div class="inner-content">
-            <address class="v-middle">
-              <span>Egypt</span>
-              <span>Damietta,El-Mahalla El-Kubra,</span>
-              <span>01024552406 , 01004392260</span>
-            </address>
+          <div class="nav-content">
+            <div class="inner-content">
+              <address class="v-middle">
+                46, Edgerton Gardon , <br />NW4 4BA, London, <br />United Kingdom
+              </address>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <!-- End Nav Bar -->
+    <!-- End Nav Bar -->
 
   <main class="main-root">
     <div id="dsn-scrollbar">
@@ -292,7 +263,7 @@
         <section class="contact-up section-margin section-padding">
           <div class="container">
             <div class="c-wapp">
-              <a href="contact.html" class="effect-ajax">
+              <a href="site/contact.html" class="effect-ajax">
                 <span class="hiring"> Contact Us </span>
                 <span class="career"> Let's talk </span>
               </a>
@@ -358,6 +329,6 @@
   <script src="{{ asset('/site/assets/js/custom.js') }}"></script>
 </body>
 
-<!-- Mirrored from template.dsngrid.com/JinnityAI/dark/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 12 Jul 2025 15:58:22 GMT -->
+<!-- Mirrored from template.dsngrid.com/JinnityAI/dark/site/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 12 Jul 2025 15:58:22 GMT -->
 
 </html>
